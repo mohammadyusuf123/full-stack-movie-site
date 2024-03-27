076450b1dcdf4a5f990921a68384d1d854acd3bf
Zayn Yusuf <yusue786@gmail.com>
2024-03-27 17:59:08 +0600

    adding explore page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,9 +3,11 @@ import { useParams } from "react-router-dom";
 import "./style.scss"
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Casting from "./casting/Casting";
+
 const Details = () => {
     const {mediaType,id}=useParams()
-    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data,loading } = useFetch(`/${mediaType}/${id}/videos`);
     const { data: credits, loading: creditsLoading } = useFetch(
         `/${mediaType}/${id}/credits`
     );
@@ -14,6 +16,7 @@ const Details = () => {
     return (
         <div>
            <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
+           <Casting data={credits?.cast} loading={creditsLoading} />
         </div>
     );
 };
